bfae647f551da700577311081768ab832e053dd1
Minnena5678 <142718210+Minnena5678@users.noreply.github.com>
2024-03-27 13:12:52 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Name Projact</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <div class="login-container">
+        <h1><center>Login</center></h1>
+    <form method="post">  
+        <center>
+        <label for="username">User : </label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password : </label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <button type="submit" class="button" name="login">Login</button>
+        </center>
+    </form>  
+</div>
+    </body>
+</html>
